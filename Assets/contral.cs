019ed84999a91a5a4789d11054f1e7413f28170e
xiaoyue1-1 <3594469083@qq.com>
2026-02-15 13:27:37 +0800

    try

--- Assets/contral.cs
+++ Assets/contral.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class contral : MonoBehaviour
 {
+    public bool IsClecken;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void Try()
+    {
+        IsClecken = true; 
     }
 }
